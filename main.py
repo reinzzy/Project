de8f230a6cf002ea4b70bc04eef1b25ad009b59f
Otto <ottosantoso11@gmail.com>
2024-10-16 09:42:17 +0700

    update minor

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 from kivy.app import App
-from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.uix.popup import Popup
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.label import Label
-from kivy.uix.button import Button
+from kivy.uix.screenmanager import ScreenManager
 from kivy.config import Config
 from kivy.core.window import Window
 from screen.login_screen import LoginScreen
