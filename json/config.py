c80aec02a9c955f9b435019fa19e5846d542342d
Otto <ottosantoso11@gmail.com>
2024-10-27 13:30:06 +0700

    upload config

---
+++ json/config.py
@@ -0,0 +1,9 @@
+firebase_config = {
+    "apiKey": "AIzaSyCMDIZ_s0HG3Ozh_1tccSCaWmXC-0kZo1Y",
+    "authDomain": "projectpython-58225.firebaseapp.com",
+    "databaseURL": "https://projectpython-58225-default-rtdb.asia-southeast1.firebasedatabase.app",
+    "projectId": "projectpython-58225",
+    "storageBucket": "projectpython-58225.appspot.com",
+    "messagingSenderId": "635697293104",
+    "appId": "1:635697293104:android:afe47f84df46cc64020af5"
+}
